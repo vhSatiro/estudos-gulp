5a157ff90397fc81cf9a929171734f514709a49c
Vinicius Sátiro <vinicius.satiro@madeinweb.combr>
2018-06-05 12:50:48 -0300

    Creation of the gulp tasks.

--- README.md
+++ README.md
@@ -9,4 +9,18 @@ source files, pre-processed, un-minified.
 development files, pre-processed, un-minified. The directory where you will be running the web server.  
 
 ### dist
-production files, processed, minified.  
+production files, processed, minified.
+
+#### Steps
+
+1. Create the paths variable on gulpfile.js.
+2. Create the task to copy files of each type to tmp folder using pipe(), then, combine all of then in an 'copy' function.
+3. Create an injection to tell to index.html which CSS and JS files he will use, using gulp-inject.
+4. Create the serve task to run the server and reload on any changes, use inject as an dependency, this will copy everything that is changed on src to tmp.
+5. Create the watch task, it will use serve as an dependency, this means it will start the server and then start watching the changes on the src folder, if something is changed, it will trigger the inject task and inject the changed files. (Link the watch task as an dependency of default task, with this, we can start watching with the command 'gulp').
+6. Create the tasks to minify and copy to dist ':dist' and create an 'copy:dist' to unite all of this tasks.
+7. Create the build task and use the inject:dist as an dependency.
+8. Add the HTMLclean tags over the comments on index.html <!--[htmlclean-protect]--> <!--[/htmlclean-protect]-->.
+  
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,100 @@
 var gulp = require('gulp');
+var inject = require('gulp-inject');
+var webserver = require('gulp-webserver');
+var htmlclean = require('gulp-htmlclean');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var del = require('del');
 
-gulp.task('default', function () { 
-    console.log('Hello BINAS!');
+var paths = {
+    src: 'src/**/*',
+    srcHTML: 'src/**/*.html',
+    srcCSS: 'src/**/*.css',
+    srcJS: 'src/**/*.js',
+
+    tmp: 'tmp',
+    tmpIndex: 'tmp/index.html',
+    tmpCSS: 'tmp/**/*.css',
+    tmpJS: 'tmp/**/*.js',
+
+    dist: 'dist',
+    distIndex: 'dist/index.html',
+    distCSS: 'dist/**/*.css',
+    distJS: 'dist/**/*.js'
+};
+
+gulp.task('default', ['watch']);
+
+gulp.task('html', function () {
+    return gulp.src(paths.srcHTML).pipe(gulp.dest(paths.tmp));
 });
 
+gulp.task('css', function () {
+    return gulp.src(paths.srcCSS).pipe(gulp.dest(paths.tmp));
+});
+
+gulp.task('js', function () {
+    return gulp.src(paths.srcJS).pipe(gulp.dest(paths.tmp));
+});
+
+gulp.task('copy', ['html', 'css', 'js']);
+
+gulp.task('inject', ['copy'], function () {
+    //copy is added as an dependency of inject, this means, that gulp will first execute copy, then inject.
+    var css = gulp.src(paths.tmpCSS);
+    var js = gulp.src(paths.tmpJS);
+    return gulp.src(paths.tmpIndex)
+        .pipe(inject(css, { relative: true }))
+        .pipe(inject(js, { relative: true }))
+        .pipe(gulp.dest(paths.tmp));
+});
+
+gulp.task('serve', ['inject'], function () {
+    return gulp.src(paths.tmp)
+        .pipe(webserver({
+            port: 3000,
+            livereload: true
+        }));
+});
+
+gulp.task('watch', ['serve'], function () {
+    gulp.watch(paths.src, ['inject']);
+});
+
+gulp.task('html:dist', function () {
+    return gulp.src(paths.srcHTML)
+        .pipe(htmlclean())
+        .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('css:dist', function () {
+    return gulp.src(paths.srcCSS)
+        .pipe(concat('style.min.css'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('js:dist', function () {
+    return gulp.src(paths.srcJS)
+        .pipe(concat('script.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('copy:dist', ['html:dist', 'css:dist', 'js:dist']);
+
+gulp.task('inject:dist', ['copy:dist'], function () {
+    var css = gulp.src(paths.distCSS);
+    var js = gulp.src(paths.distJS);
+    return gulp.src(paths.distIndex)
+        .pipe(inject(css, { relative: true }))
+        .pipe(inject(js, { relative: true }))
+        .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('build', ['inject:dist']);
+
+gulp.task('clean', function () {
+    del([paths.tmp, paths.dist]);
+});
